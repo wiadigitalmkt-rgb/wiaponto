a8bef8da270373389118338404baeeffa8badbd1
wiadigitalmkt-rgb <wiadigitalmkt@gmail.com>
2026-08-09 14:08:27 -0300

    Update Contratos.jsx

--- src/pages/admin/Contratos.jsx
+++ src/pages/admin/Contratos.jsx
@@ -32,7 +32,7 @@ export default function Contratos() {
       }
     } catch (err) {
       console.error('Erro ao buscar contratos:', err);
-    } font-finally {
+    } finally {
       setLoading(false);
     }
   };

6e33fd4e3407c161e87f68a0929715f2615a04be
wiadigitalmkt-rgb <wiadigitalmkt@gmail.com>
2026-08-11 01:00:27 -0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Eye, EyeOff, ArrowRight, LogIn } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from '../lib/supabase';
 import bgLoginImg from './bgloginponto.png';
 
 export default function Login() {

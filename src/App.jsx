d44acf51959ab62441a906e6d9dfc7e1b8b9fdf2
wiadigitalmkt-rgb <wiadigitalmkt@gmail.com>
2026-08-12 02:28:47 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import AdminDashboard from './pages/admin/Dashboard';
 import Employees from './pages/admin/Employees';
 import CompanySettings from './pages/admin/CompanySettings';
 import PontoEletronico from './pages/admin/Ponto';
-import Usuario from './pages/admin/Usuarios';
+import Usuario from './pages/admin/Usuario';
 import Admissao from './pages/admin/Admissao';
 
 // Imports das Páginas Admin Criadas
@@ -92,7 +92,7 @@ const AuthenticatedApp = () => {
         <Route path="/admin/ponto" element={<PontoEletronico />} />
         <Route path="/admin/colaboradores" element={<Employees />} />
         <Route path="/admin/empresa" element={<CompanySettings />} />
-        <Route path="/admin/usuarios" element={<Usuarios />} />
+        <Route path="/admin/usuarios" element={<Usuario />} />
         <Route path="/admin/admissao" element={<Admissao />} />
         <Route path="/admin/contratos" element={<Contratos />} />
         <Route path="/admin/banco-horas" element={<Banco />} />

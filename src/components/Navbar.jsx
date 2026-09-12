ce24b1f83f9fac0cc23c7db0b28bb8bb24163035
wiadigitalmkt-rgb <wiadigitalmkt@gmail.com>
2026-09-12 17:26:25 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -100,7 +100,7 @@ export default function Navbar({ selectedCompany = 'PontoMax' }) {
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="start" className="bg-white text-slate-800">  
                   <DropdownMenuItem className="cursor-pointer hover:!bg-[#1a2c6a] hover:!text-white focus:!bg-[#1a2c6a] focus:!text-white data-[highlighted]:!bg-[#1a2c6a] data-[highlighted]:!text-white" onClick={() => navigate('/admin/ponto')}>
-                    Ponto Eletrônico
+                    Espeçho de ponto
                   </DropdownMenuItem> 
                   <DropdownMenuItem className="cursor-pointer hover:!bg-[#1a2c6a] hover:!text-white focus:!bg-[#1a2c6a] focus:!text-white data-[highlighted]:!bg-[#1a2c6a] data-[highlighted]:!text-white" onClick={() => navigate('/admin/colaboradores')}>
                     Usuários
